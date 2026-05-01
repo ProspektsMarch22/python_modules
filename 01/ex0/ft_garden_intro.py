51e660017f5015e13407bf6815b0bd6540b50376
Isaac Barbosa <isaaccezar22@gmail.com>
2026-05-01 19:34:48 +0100

    Deletes old project files - subject has changed. Adds new 01 folder, adds ex0

--- 01/ex0/ft_garden_intro.py
+++ 01/ex0/ft_garden_intro.py
@@ -1,13 +1,15 @@
 def main() -> None:
+    print("=== Welcome to My Garden ===")
     name = "rose"
     height = 25
     age = 30
-    print("=== Welcome to My Garden ===")
-    print(f"Plant: {name.capitalize()} ")
-    print(f"Height: {height}cm ")
-    print(f"{age} days")
+    print(f"Plant: {name.capitalize()}",
+          f"Height: {height}cm",
+          f"Age: {age} days",
+          sep="\n")
     print("\n=== End of Program ===")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
