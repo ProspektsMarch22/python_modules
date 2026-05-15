d3e9d046284f1e2bc16f335abb57682ce1976b3e
Isaac Barbosa <isaaccezar22@gmail.com>
2026-05-15 23:51:32 +0100

    adds ex3, compliant, removed all cache files encountered

--- 03/ex3/ft_achievement_tracker.py
+++ 03/ex3/ft_achievement_tracker.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
+
 import random as rd
+
 
 def get_player_achievements(ach_nbr: int) -> set:
     ach_list: list = [
-    "Crafting Genius",
-    "World Savior",
-    "Master Explorer",
-    "Collector Supreme",
-    "Untouchable",
-    "Boss Slayer",
-    "Strategist",
-    "Unstoppable",
-    "Speed Runner",
-    "Survivor",
-    "Treasure Hunter",
-    "First Steps",
-    "Sharp Mind",
+        "Crafting Genius",
+        "World Savior",
+        "Master Explorer",
+        "Collector Supreme",
+        "Untouchable",
+        "Boss Slayer",
+        "Strategist",
+        "Unstoppable",
+        "Speed Runner",
+        "Survivor",
+        "Treasure Hunter",
+        "First Steps",
+        "Sharp Mind",
     ]
     ach_set: set = set()
     for i in range(ach_nbr):
@@ -26,19 +28,45 @@ def get_player_achievements(ach_nbr: int) -> set:
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
-    alice, bob, charlie, dylan = (get_player_achievements(10) for _ in range(4))
+    #
+    # Iniializes player achievements sets
+    alice, bob = (get_player_achievements(10) for _ in range(2))
+    charlie, dylan = (get_player_achievements(10) for _ in range(2))
+    #
+    # Prints player sets
     print("Player Alice:", alice, end="\n\n")
     print("Player Bob:", bob, end="\n\n")
     print("Player Charlie:", charlie, end="\n\n")
     print("Player Dylan:", dylan, end="\n\n")
-    print("All distinctive achievements:", set.union(alice, bob, charlie, dylan))
-    print("\nCommon achievements:", set.intersection(alice, bob, charlie, dylan))
-    print("\nOnly Alice has:", set.difference(alice, set.union(bob, charlie, dylan)))
-    print("Only Bob has:", set.difference(bob, set.union(alice, charlie, dylan)))
-    print("Only Charlie has:", set.difference(charlie, set.union(alice, bob, dylan)))
-    print("Only Dylan has:", set.difference(dylan, set.union(alice, bob, charlie)))
+    #
+    # Prints all elements within union of sets, uniques
+    print("All distinctive achievements:",
+          set.union(alice, bob, charlie, dylan))
+    #
+    # Intersection between player sets
+    print("\nCommon achievements:",
+          set.intersection(alice, bob, charlie, dylan))
+    #
+    # Difference between player set and union of rest of players' sets
+    print("\nOnly Alice has:",
+          set.difference(alice, set.union(bob, charlie, dylan)))
+    print("Only Bob has:",
+          set.difference(bob, set.union(alice, charlie, dylan)))
+    print("Only Charlie has:",
+          set.difference(charlie, set.union(alice, bob, dylan)))
+    print("Only Dylan has:",
+          set.difference(dylan, set.union(alice, bob, charlie)))
+    #
+    # Difference between union of rest of players' sets and player set
+    print("\nAlice is missing:",
+          set.difference(set.union(bob, charlie, dylan), alice))
+    print("Bob is missing:",
+          set.difference(set.union(alice, charlie, dylan), bob))
+    print("Charlie is missing:",
+          set.difference(set.union(alice, bob, dylan), charlie))
+    print("Dyaln is missing:",
+          set.difference(set.union(alice, bob, charlie), dylan))
 
 
 if __name__ == '__main__':
     main()
-
