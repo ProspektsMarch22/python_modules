d8744aa369af770ead2a069dbf7b32c05b98c9f6
Isaac Barbosa <isaaccezar22@gmail.com>
2026-05-18 03:53:07 +0100

    adds ex1, mypy flake8 compliant

---
+++ 04/ex1/ft_archive_creation.py
@@ -0,0 +1,54 @@
+#!/bin/usr/env python3
+
+from sys import argv
+from typing import IO
+
+
+def display_file_data() -> None:
+    file_names: list[str] = argv[1:]
+    if (len(file_names) == 0):
+        print("Usage: ft_ancienty_text.py <file1>, <file2>, ...")
+        return
+    print("=== Cyber Archives Recovery & Preservation ===")
+    for file in file_names:
+        try:
+            f: IO[str] | None = None
+            print(f"Accessing file '{file}'")
+            f = open(file)
+            content: str = f.read()
+        except OSError as e:
+            print(f"Error opening file '{file}':", e)
+        else:
+            print("-" * 3, end="\n\n")
+            print(content)
+            print("-" * 3)
+        finally:
+            if f is not None:
+                f.close()
+                print(f"File '{file}' closed.")
+        if len(content) > 0:
+            print("\nTransform data:")
+            print("-" * 3, end="\n\n")
+            lines: list[str] = content.split("\n")
+            lines.pop(-1)
+            for i in range(len(lines)):
+                lines[i] = lines[i] + "#\n"
+                print(lines[i], end="")
+            print("\n" + "-" * 3)
+            new_file: str = input("Enter new file name (or empty): ")
+            if (len(new_file) > 0):
+                print(f"Saving data to '{new_file}'")
+                n_f: IO[str] = open(new_file, "w")
+                for line in lines:
+                    n_f.write(line)
+                print(f"Data saved in file '{new_file}'")
+            else:
+                print("Not saving data.")
+
+
+def main() -> None:
+    display_file_data()
+
+
+if __name__ == '__main__':
+    main()
