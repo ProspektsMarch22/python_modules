86aa56bf0b9ac92af9975699ac9edb33187e1861
Isaac Barbosa <isaaccezar22@gmail.com>
2026-06-12 02:13:36 +0100

    Adds PM06 part 2, modified version of alchemy/__init__.py file, catching changes on alchemy/elements.py file but couldn't figure out what's diff.

---
+++ 06/ft_distillation_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from alchemy.potions import healing_potion, strength_potion
+
+def main() -> None:
+    print("=== Distillation 0 ===")
+    print("Testing strength_potion:", strength_potion())
+    print("Testing healing_potion:", healing_potion())
+
+
+if __name__ == '__main__':
+    main()
