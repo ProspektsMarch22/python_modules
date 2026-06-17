5f14b3c138ec73858c44d815c9b98996c0f9113c
Isaac Barbosa <isaaccezar22@gmail.com>
2026-06-17 21:45:31 +0100

    Complete creature module with creature abstract and concrete classes, test suite on the module, compliant to mypy and flake8

--- 07/ex0/creature.py
+++ 07/ex0/creature.py
@@ -19,4 +19,61 @@ class Creature(ABC):
         pass
 
     def describe(self) -> str:
-        return f'{self.name} is a {self.type}'
+        return f'{self.name()} is a {self.type()} type Creature'
+
+
+class Flameling(Creature):
+    def name(self) -> str:
+        return "Flameling"
+
+    def type(self) -> str:
+        return "Fire"
+
+    def attack(self) -> str:
+        return f'{self.name()} uses Ember!'
+
+
+class Pyrodon(Creature):
+    def name(self) -> str:
+        return "Pyrodon"
+
+    def type(self) -> str:
+        return "Fire/Flying"
+
+    def attack(self) -> str:
+        return f'{self.name()} uses Flamethrower!'
+
+
+class Aquabub(Creature):
+    def name(self) -> str:
+        return "Aquabub"
+
+    def type(self) -> str:
+        return "Water"
+
+    def attack(self) -> str:
+        return f'{self.name()} uses Water Gun!'
+
+
+class Torragon(Creature):
+    def name(self) -> str:
+        return "Torragon"
+
+    def type(self) -> str:
+        return "Water"
+
+    def attack(self) -> str:
+        return f'{self.name()} uses Hydro Pump!'
+
+
+if __name__ == '__main__':
+    """ Testing suite for the creatures """
+    pocket_monsters: list[Creature] = [
+        Flameling(),
+        Pyrodon(),
+        Aquabub(),
+        Torragon()
+    ]
+    for mon in pocket_monsters:
+        print(mon.describe())
+        print(mon.attack())
